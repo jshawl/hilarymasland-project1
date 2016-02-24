99ea3900d34112884da96eb0d4299fadb9da1621
Hilary Masland <hilarymasland@gmail.com>
2016-02-24 09:17:53 -0500

    Initial commit - moved from starter page

---
+++ script.js
@@ -0,0 +1,21 @@
+
+  function questionOne() {
+    var nickname = prompt("True or False: One of my nicknames is Grizzly.");
+      if (nickname === 'True') {
+        alert("Correct - I'm a bear not a tiger");
+      }
+      else {
+        alert("Wrong-O");
+      }
+  }
+
+
+  function questionTwo() {
+    var state= prompt("True or False: I am from Ohio.");
+      if (state === 'False') {
+        alert("PA PRIDE, Y'ALL");
+      }
+      else {
+        alert("PA PRIDE, Y'ALL");
+      }
+  }
