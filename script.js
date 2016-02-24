8ec6faba92c97ca9f19c73156c7fc5af41927724
Hilary Masland <hilarymasland@gmail.com>
2016-02-24 14:49:52 -0500

    Cleaned up the code

--- script.js
+++ script.js
@@ -1,48 +1,59 @@
 
+var questions=[
+
+  {
+    promptText:"True or False: One of my nicknames is Grizzly.",
+    answer:"True",
+    correctResponse:"Correct",
+    wrongResponse:"Wrong"
+  },
+  {
+    promptText:"True or False: I am from Ohio",
+    answer:"False",
+    correctResponse:"Correct- PA PRIDE, Y'ALL",
+    wrongResponse:"Wrong- PA PRIDE, Y'ALL"
+  },
+  {
+    promptText:"True or False: My favorite move is Titanic.",
+    answer:'True',
+    correctResponse:"Correct- I'll never let go",
+    wrongResponse:"Wrong"
+  },
+  {
+    promptText:"True or False: I LOVE Broadway / Musicals.",
+    answer:'True',
+    correctResponse:"Correct- If only I could sing....",
+    wrongResponse:"Wrong"
+  },
+  {
+    promptText:"True or False: I LOVE Broadway / Musicals.",
+    answer:'True',
+    correctResponse:"Correct- If only I could sing....",
+    wrongResponse:"Wrong"
+  }
+];
+
+questions.forEach(function(question,index){
+  var button = document.createElement("button")
+  button.innerHTML="Question " + index
+  button.addEventListener("click", function(){
+    
+  })
+  document.querySelector("div.buttons").appendChild(button);
+  console.log(index);
+})
+
+
+
 var gameScore = 0;
 
-  function questionOne() {
-    var nickname = prompt("True or False: One of my nicknames is Grizzly.");
-      if (nickname === 'True') {
-        alert("Correct");
+  function question(index) {
+    var nickname = prompt(questions[index].promptText);
+      if (nickname === questions[index].answer) {
+        alert(questions[index].correctResponse);
         gameScore++;
       }
       else {
-        alert("Wrong");
-      }
-  }
-
-  function questionTwo() {
-    var state= prompt("True or False: I am from Ohio.");
-      if (state === 'False') {
-        alert("Correct- PA PRIDE, Y'ALL");
-        gameScore++;
-      }
-      else {
-        alert("Wrong- PA PRIDE, Y'ALL");
-      }
-  }
-
-  function questionThree() {
-    var movie = prompt("True or False: My favorite move is Titanic.");
-      if (movie === 'True') {
-        alert("Correct- I'll never let go");
-        gameScore++;
-
-      }
-      else {
-        alert("Wrong");
-      }
-  }
-
-  function questionFour() {
-    var broadway = prompt("True or False: I LOVE Broadway / Musicals.");
-      if (broadway === 'True') {
-        alert("Correct- If only I could sing....");
-        gameScore++;
-      }
-      else {
-        alert("Wrong");
-        alert(gameScore);
+        alert(questions[index].wrongResponse);
       }
   }
